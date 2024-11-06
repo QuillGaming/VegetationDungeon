18bbe3b74ee7a289f2fa558aa69261baf432e5e5
squirtmuffin1 <NathanielCav@gmail.com>
2024-11-06 14:11:27 -0500

    Animation has been added
    So I was able to get the basic animation working relatively well.
    
    It has some extra assets that I have not deleted yet but I'll get there.
    
    Combat does not yet work

--- Assets/playerScript.cs
+++ Assets/playerScript.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
+using System.Collections;
 
 public class playerScript : MonoBehaviour
 {
     Vector2 mv;
     public float playerSpeed;
     public float yRot;
+    public TMPro.TMP_Text HUD;
+
+    bool enemyIsHitable = false;
+    float animationDelay = 0f;
+    int enemyHealth = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,6 +30,30 @@ public class playerScript : MonoBehaviour
         {
             GameObject.Find("Player").GetComponent<Animator>().SetBool("isWalking", false);
         }
+
+        if (Input.GetMouseButtonDown(0) && animationDelay <= 0.0f) // should be left click
+        {
+            Debug.Log("You punched");
+            GameObject.Find("Player").GetComponent<Animator>().SetBool("isPunching", true);
+            animationDelay = 1f;
+            if (enemyIsHitable)
+            {
+                enemyHealth -= 2;
+                HUD.text = "enemy health: " + enemyHealth;
+            }
+        }
+        animationDelay -= Time.deltaTime;
+        if (animationDelay <= 0.0f)
+        {
+            Debug.Log("punch done");
+            GameObject.Find("Player").GetComponent<Animator>().SetBool("isPunching", false);
+        }
+
+        //enemy death
+        if (enemyHealth <= 0)
+        {
+            GameObject.Find("Enemy_test").GetComponent<UnityEngine.AI.NavMeshAgent>().isStopped = true; //this should stop the agent's movement upon death
+        }
     }
 
     void OnMove(InputValue move)
@@ -38,4 +69,45 @@ public class playerScript : MonoBehaviour
             transform.position += transform.forward * mv.y * playerSpeed;
         }
     }
+
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "to_Hit_Box")
+        {
+            Debug.Log("enemy is in box");
+            enemyIsHitable = true;
+        }
+    }
+
+    /*void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+        if (collision.relativeVelocity.magnitude > 2)
+        {
+            Debug.Log("enemy is in box");
+            enemyIsHitable = true;
+        }
+    }*/
+
+    private void OnTriggerExit(Collider other2)
+    {
+        if (other2.gameObject.name == "to_Hit_Box")
+        {
+            Debug.Log("enemy is not in box");
+            enemyIsHitable = false;
+        }
+    }
+    /*void onTriggerStay (Collider hitArea)
+    {
+        if (hitArea.gameObject.name == "to_Hit_Box")
+        {
+            enemyIsHitable = true;
+            Debug.Log("enemy is in box");
+        }
+    }*/
 }
