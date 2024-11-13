d801570d3ef6bc9d4e24a6d541b010ee3952bccc
squirtmuffin1 <NathanielCav@gmail.com>
2024-11-13 16:55:57 -0500

    Camera panning out works
    changed the desiredDistance in camera code to pan out camera upon player death
    
    also fixed player collider messing up running into walls with combat cube in front of player

--- Assets/cameraScript.cs
+++ Assets/cameraScript.cs
@@ -34,7 +34,7 @@ public class cameraScript : MonoBehaviour
     private float correctedDistance;
     private float mouseAxis;
 
-    private bool notDead = true; //this is mine
+    private bool notDead = true; //this is mine - Nathaniel
 
     void Start()
     {
@@ -53,75 +53,78 @@ public class cameraScript : MonoBehaviour
      */
     void LateUpdate()
     {
-        Vector3 vTargetOffset;
-
-        // Don't do anything if target is not defined
-        if (!target)
-            return;
-
-        // Ease behind the target if any of the directional keys are pressed
-        if (GUIUtility.hotControl == 0)
-        {
-            if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
-            {
-                float targetRotationAngle = target.eulerAngles.y;
-                float currentRotationAngle = transform.eulerAngles.y;
-                xDeg = Mathf.LerpAngle(currentRotationAngle, targetRotationAngle, rotationDampening * Time.deltaTime);
-            }
-        }
-
-
-        // calculate the desired distance
-        desiredDistance -= mouseAxis * Time.deltaTime * zoomRate * Mathf.Abs(desiredDistance) * speedDistance;
-        desiredDistance = Mathf.Clamp(desiredDistance, minDistance, maxDistance);
-
-        yDeg = ClampAngle(yDeg, yMinLimit, yMaxLimit);
-
-        // set camera rotation
-        Quaternion rotation = Quaternion.Euler(yDeg, xDeg, 0);
-        correctedDistance = desiredDistance;
-
-        // calculate desired camera position
-        vTargetOffset = new Vector3(0, -targetHeight, 0);
-        Vector3 position = target.position - (rotation * Vector3.forward * desiredDistance + vTargetOffset);
-
-        // check for collision using the true target's desired registration point as set by user using height
-        RaycastHit collisionHit;
-        Vector3 trueTargetPosition = new Vector3(target.position.x, target.position.y, target.position.z) - vTargetOffset;
-
-        // if there was a collision, correct the camera position and calculate the corrected distance
-        bool isCorrected = false;
-        if (Physics.Linecast(trueTargetPosition, position, out collisionHit, collisionLayers.value))
-        {
-            // calculate the distance from the original estimated position to the collision location,
-            // subtracting out a safety "offset" distance from the object we hit.  The offset will help
-            // keep the camera from being right on top of the surface we hit, which usually shows up as
-            // the surface geometry getting partially clipped by the camera's front clipping plane.
-            correctedDistance = Vector3.Distance(trueTargetPosition, collisionHit.point) - offsetFromWall;
-            isCorrected = true;
-        }
-
-        // For smoothing, lerp distance only if either distance wasn't corrected, or correctedDistance is more than currentDistance
-        currentDistance = !isCorrected || correctedDistance > currentDistance ? Mathf.Lerp(currentDistance, correctedDistance, Time.deltaTime * zoomDampening) : correctedDistance;
-
-        // keep within legal limits
-        currentDistance = Mathf.Clamp(currentDistance, minDistance, maxDistance);
-
-        // recalculate position based on the new currentDistance
-        position = target.position - (rotation * Vector3.forward * currentDistance + vTargetOffset);
-
-        transform.rotation = rotation;
-        transform.position = position;
-
-        // this is the code that I want to use to move the camera back upon the player's death
+        // this is the code that I want to use to move the camera back upon the player's death - Nathaniel
         if (GameObject.Find("Player").GetComponent<Animator>().GetBool("isDead") && notDead)
         {
             //this moves the camera
             Debug.Log("the player is dead");
-            GameObject.Find("Main Camera").GetComponent<Transform>().localPosition += new Vector3(0, 0, 4);
+            desiredDistance += 5f;
             notDead = false;
         }
+        else if(!GameObject.Find("Player").GetComponent<Animator>().GetBool("isDead") || !notDead)
+        {
+            Vector3 vTargetOffset;
 
+            // Don't do anything if target is not defined
+            if (!target)
+                return;
+
+            // Ease behind the target if any of the directional keys are pressed
+            if (GUIUtility.hotControl == 0)
+            {
+                if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+                {
+                    float targetRotationAngle = target.eulerAngles.y;
+                    float currentRotationAngle = transform.eulerAngles.y;
+                    xDeg = Mathf.LerpAngle(currentRotationAngle, targetRotationAngle, rotationDampening * Time.deltaTime);
+                }
+            }
+
+
+            // calculate the desired distance
+            desiredDistance -= mouseAxis * Time.deltaTime * zoomRate * Mathf.Abs(desiredDistance) * speedDistance;
+            desiredDistance = Mathf.Clamp(desiredDistance, minDistance, maxDistance);
+
+            yDeg = ClampAngle(yDeg, yMinLimit, yMaxLimit);
+
+            // set camera rotation
+            Quaternion rotation = Quaternion.Euler(yDeg, xDeg, 0);
+            correctedDistance = desiredDistance;
+
+            // calculate desired camera position
+            vTargetOffset = new Vector3(0, -targetHeight, 0);
+            Vector3 position = target.position - (rotation * Vector3.forward * desiredDistance + vTargetOffset);
+
+            // check for collision using the true target's desired registration point as set by user using height
+            RaycastHit collisionHit;
+            Vector3 trueTargetPosition = new Vector3(target.position.x, target.position.y, target.position.z) - vTargetOffset;
+    
+            // if there was a collision, correct the camera position and calculate the corrected distance
+            bool isCorrected = false;
+            if (Physics.Linecast(trueTargetPosition, position, out collisionHit, collisionLayers.value))
+            {
+                // calculate the distance from the original estimated position to the collision location,
+                // subtracting out a safety "offset" distance from the object we hit.  The offset will help
+                // keep the camera from being right on top of the surface we hit, which usually shows up as
+                // the surface geometry getting partially clipped by the camera's front clipping plane.
+                correctedDistance = Vector3.Distance(trueTargetPosition, collisionHit.point) - offsetFromWall;
+                isCorrected = true;
+            }
+
+            // For smoothing, lerp distance only if either distance wasn't corrected, or correctedDistance is more than currentDistance
+            currentDistance = !isCorrected || correctedDistance > currentDistance ? Mathf.Lerp(currentDistance, correctedDistance, Time.deltaTime * zoomDampening) : correctedDistance;
+
+            // keep within legal limits
+            currentDistance = Mathf.Clamp(currentDistance, minDistance, maxDistance);
+
+            // recalculate position based on the new currentDistance
+            position = target.position - (rotation * Vector3.forward * currentDistance + vTargetOffset);
+
+            transform.rotation = rotation;
+            transform.position = position;
+
+        }
+        
     }
 
     private static float ClampAngle(float angle, float min, float max)
