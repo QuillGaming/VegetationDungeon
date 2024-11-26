7ba29c2a4ea801c238187ec95ac2fe98c8e1af00
squirtmuffin1 <NathanielCav@gmail.com>
2024-11-26 16:22:23 -0500

    two weapon version of inventory is complete
    - added back versions of the weapons
    - made code that allows swapping of the weapons if you have both
    - fixed bug that allowed for swapping during a hit

--- Assets/itemScript.cs
+++ Assets/itemScript.cs
@@ -4,9 +4,11 @@ public class itemScript : MonoBehaviour
 {
     public GameObject swordFloor;
     public GameObject swordHeld;
+    public GameObject swordBack;
 
     public GameObject halberdFloor;
     public GameObject halberdHeld;
+    public GameObject halberdBack;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -14,6 +16,9 @@ public class itemScript : MonoBehaviour
     {
         swordHeld.SetActive(false);
         halberdHeld.SetActive(false);
+
+        swordBack.SetActive(false);
+        halberdBack.SetActive(false);
     }
 
     // Update is called once per frame
--- Assets/playerScript.cs
+++ Assets/playerScript.cs
@@ -17,12 +17,18 @@ public class playerScript : MonoBehaviour
     bool enemyIsHitable = false;
     bool playerIsHitable = false;
     bool healthDisplayDelay = false;
+
+    bool halberd = false;
+    bool sword = false;
+
     float animationDelay = 0f;
     float weaponDelay = 0.9f;
     float enemyAttackCooldown = 1f; //was 2.5 before but was too slow for tests
+
     float backSpeed = 0.035f;
     float walkSpeed = 0.05f;
     float runSpeed = 0.085f;
+
     int enemyHealth = 10;
     int playerHealth = 20;
     int damageOuput = 2;
@@ -90,6 +96,7 @@ public class playerScript : MonoBehaviour
                 enemyIsHitable = false;
             }
         }
+        
         animationDelay -= Time.deltaTime;
         if (animationDelay <= 0.0f)
         {
@@ -100,6 +107,31 @@ public class playerScript : MonoBehaviour
                 healthDisplayDelay = false;
             }
             GameObject.Find("Player").GetComponent<Animator>().SetBool("isPunching", false);
+
+            //weapon swapping
+            if (sword && halberd && Input.GetKeyDown("f"))
+            {
+                if (GameObject.Find("Player").GetComponent<Animator>().GetBool("hasSword"))
+                {
+                    Items.GetComponent<itemScript>().swordHeld.SetActive(false);
+                    Items.GetComponent<itemScript>().swordBack.SetActive(true);
+                    GameObject.Find("Player").GetComponent<Animator>().SetBool("hasSword", false);
+
+                    Items.GetComponent<itemScript>().halberdHeld.SetActive(true);
+                    Items.GetComponent<itemScript>().halberdBack.SetActive(false);
+                    GameObject.Find("Player").GetComponent<Animator>().SetBool("hasHalberd", true);
+                }
+                else
+                {
+                    Items.GetComponent<itemScript>().halberdHeld.SetActive(false);
+                    Items.GetComponent<itemScript>().halberdBack.SetActive(true);
+                    GameObject.Find("Player").GetComponent<Animator>().SetBool("hasHalberd", false);
+
+                    Items.GetComponent<itemScript>().swordHeld.SetActive(true);
+                    Items.GetComponent<itemScript>().swordBack.SetActive(false);
+                    GameObject.Find("Player").GetComponent<Animator>().SetBool("hasSword", true);
+                }
+            }
         }
 
         
@@ -119,8 +151,6 @@ public class playerScript : MonoBehaviour
             pHealthBar.GetComponent<playerHealthBar>().UpdateHealthBar(playerHealth, 20f);
             enemyAttackCooldown = 1f; //was 2.5 which was too slow for tests
         }
-
-        
 
     }
 
@@ -154,7 +184,7 @@ public class playerScript : MonoBehaviour
         }
 
         //pick up the sword
-        if (other.gameObject.name == "pick_sword" && !GameObject.Find("Player").GetComponent<Animator>().GetBool("hasSword"))
+        if (other.gameObject.name == "pick_sword" && !sword)
         {
             HUD.text = "Press 'e' to pick up sword.";
             if (Input.GetKey("e"))
@@ -166,12 +196,19 @@ public class playerScript : MonoBehaviour
                 box.transform.localPosition += new Vector3(0f, 0f, 0.6f);
                 weaponDelay = 1.3f;
                 damageOuput = 3;
+                sword = true;
                 HUD.text = "";
+                if (halberd)
+                {
+                    Items.GetComponent<itemScript>().halberdHeld.SetActive(false);
+                    Items.GetComponent<itemScript>().halberdBack.SetActive(true);
+                    GameObject.Find("Player").GetComponent<Animator>().SetBool("hasHalberd", false);
+                }
             }
         }
 
         //pick up the halberd
-        if (other.gameObject.name == "pick_halberd" && !GameObject.Find("Player").GetComponent<Animator>().GetBool("hasHalberd"))
+        if (other.gameObject.name == "pick_halberd" && !halberd)
         {
             HUD.text = "Press 'e' to pick up halberd.";
             if (Input.GetKey("e"))
@@ -181,7 +218,14 @@ public class playerScript : MonoBehaviour
                 Items.GetComponent<itemScript>().halberdHeld.SetActive(true);
                 weaponDelay = 1.8f;
                 damageOuput = 4;
+                halberd = true;
                 HUD.text = "";
+                if (sword)
+                {
+                    Items.GetComponent<itemScript>().swordHeld.SetActive(false);
+                    Items.GetComponent<itemScript>().swordBack.SetActive(true);
+                    GameObject.Find("Player").GetComponent<Animator>().SetBool("hasSword", false);
+                }
             }
         }
     }
@@ -193,7 +237,7 @@ public class playerScript : MonoBehaviour
             enemyIsHitable = true;
         }
 
-        if (other1.gameObject.name == "pick_sword" && !GameObject.Find("Player").GetComponent<Animator>().GetBool("hasSword"))
+        if (other1.gameObject.name == "pick_sword" && !sword)
         {
             HUD.text = "Press 'e' to pick up sword.";
             if (Input.GetKey("e"))
@@ -205,12 +249,19 @@ public class playerScript : MonoBehaviour
                 box.transform.localPosition += new Vector3(0f, 0f, 0.6f);
                 weaponDelay = 1.3f;
                 damageOuput = 3;
+                sword = true;
                 HUD.text = "";
+                if(halberd)
+                {
+                    Items.GetComponent<itemScript>().halberdHeld.SetActive(false);
+                    Items.GetComponent<itemScript>().halberdBack.SetActive(true);
+                    GameObject.Find("Player").GetComponent<Animator>().SetBool("hasHalberd", false);
+                }
             }
         }
 
         //pick up the halberd
-        if (other1.gameObject.name == "pick_halberd" && !GameObject.Find("Player").GetComponent<Animator>().GetBool("hasHalberd"))
+        if (other1.gameObject.name == "pick_halberd" && !halberd)
         {
             HUD.text = "Press 'e' to pick up halberd.";
             if (Input.GetKey("e"))
@@ -218,9 +269,16 @@ public class playerScript : MonoBehaviour
                 GameObject.Find("Player").GetComponent<Animator>().SetBool("hasHalberd", true);
                 Items.GetComponent<itemScript>().halberdFloor.SetActive(false);
                 Items.GetComponent<itemScript>().halberdHeld.SetActive(true);
+                halberd = true;
                 weaponDelay = 1.8f;
                 damageOuput = 4;
                 HUD.text = "";
+                if (sword)
+                {
+                    Items.GetComponent<itemScript>().swordHeld.SetActive(false);
+                    Items.GetComponent<itemScript>().swordBack.SetActive(true);
+                    GameObject.Find("Player").GetComponent<Animator>().SetBool("hasSword", false);
+                }
             }
         }
     }
